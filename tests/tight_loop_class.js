6b979c923fc051328e0ecbe8037503cf433b4b2d
theMackabu <theMackabu@gmail.com>
2025-12-30 19:48:04 -0800

    add benches

---
+++ tests/tight_loop_class.js
@@ -0,0 +1,12 @@
+class Foo {
+  lookup(key) {
+    return key;
+  }
+}
+const foo = new Foo();
+for (let i = 0; i < 40000; i = i + 1) {
+  foo.lookup('a');
+  foo.lookup('b');
+  foo.lookup('c');
+}
+console.log('Done!');
---
+++ tests/tight_loop_proto.js
@@ -0,0 +1,12 @@
+function Foo() {}
+Foo.prototype.lookup = function (key) {
+  return key;
+};
+
+const foo = new Foo();
+for (let i = 0; i < 40000; i = i + 1) {
+  foo.lookup('a');
+  foo.lookup('b');
+  foo.lookup('c');
+}
+console.log('Done!');
