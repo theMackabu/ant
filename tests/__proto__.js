f9039201dc900e6fcd8ac810a1fa9f6faba7ff8c
theMackabu <theMackabu@gmail.com>
2026-01-01 03:07:51 -0800

    add __proto__ test

---
+++ tests/__proto__.js
@@ -0,0 +1,13 @@
+function ShapeA() {}
+const ShapeB = {
+  a() {
+    console.log('aaa');
+  }
+};
+
+ShapeA.prototype.__proto__ = ShapeB;
+console.log(ShapeA.prototype.__proto__); // { a: [Function: a] }
+
+const shapeA = new ShapeA();
+shapeA.a(); // aaa
+console.log(ShapeA.prototype === shapeA.__proto__); // true
