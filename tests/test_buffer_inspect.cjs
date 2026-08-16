294bad372a29f87bfe635513d65a21d4f1b88acb
theMackabu <theMackabu@gmail.com>
2026-08-16 13:36:54 -0700

    clean up IO colors

--- tests/test_buffer_inspect.cjs
+++ tests/test_buffer_inspect.cjs
@@ -16,4 +16,55 @@ assert.strictEqual(
   'Uint8Array(2) [ 72, 101 ]'
 );
 
-console.log('buffer inspection matches Node');
+function captureColored(value) {
+  const previousForceColor = process.env.FORCE_COLOR;
+  const write = process.stdout.write;
+  let output = '';
+
+  process.env.FORCE_COLOR = '1';
+  process.stdout.write = chunk => (output += chunk, true);
+  console.log(value);
+  process.stdout.write = write;
+
+  if (previousForceColor === undefined) delete process.env.FORCE_COLOR;
+  else process.env.FORCE_COLOR = previousForceColor;
+  return output;
+}
+
+assert.strictEqual(
+  captureColored(Buffer.from('Hello')),
+  '\u001b[37m<\u001b[0mBuffer ' +
+    '\u001b[33m48\u001b[0m \u001b[33m65\u001b[0m ' +
+    '\u001b[33m6c\u001b[0m \u001b[33m6c\u001b[0m ' +
+    '\u001b[33m6f\u001b[0m\u001b[37m>\u001b[0m\n'
+);
+
+const typedArrays = [
+  new Int8Array([1]),
+  new Uint8Array([1]),
+  new Uint8ClampedArray([1]),
+  new Int16Array([1]),
+  new Uint16Array([1]),
+  new Int32Array([1]),
+  new Uint32Array([1]),
+  new Float16Array([1]),
+  new Float32Array([1]),
+  new Float64Array([1]),
+  new BigInt64Array([1n]),
+  new BigUint64Array([1n]),
+];
+
+for (const value of typedArrays) {
+  const output = captureColored(value);
+  const name = value.constructor.name;
+  assert.ok(output.startsWith(`${name}(\u001b[33m1\u001b[0m)`));
+  assert.ok(output.includes(`\u001b[33m1${name.startsWith('Big') ? 'n' : ''}\u001b[0m`));
+}
+
+assert.ok(
+  captureColored(Uint8Array.from([10, 255]).buffer).includes(
+    '\u001b[37m<\u001b[33m0a\u001b[0m \u001b[33mff\u001b[0m\u001b[37m>'
+  )
+);
+
+console.log('buffer inspection formatting and colors passed');
